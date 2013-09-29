b340c2d47232ecb1f82611e6c5d101caa932d19b
Nguyễn Phan Hải <hainp2604@gmail.com>
2013-09-30 01:12:00 +0700

    Refactor query func

--- result.rb
+++ result.rb
@@ -1,31 +1,36 @@
 # user1 -> 1st query -> query 
-
 require 'octokit'
 require 'set'
 
 $client = Octokit::Client.new :login => "lewtds", :password => "1234567a"
-
 $my_set = Set.new
-users = gets.chomp
-def query(users)
-	followers = $client.followers("#{users}")[0..2].map { |follower|
-    		follower[:login]
-}
-    
-	followers.each { |username|
-	      
-    		user = $client.user username
-	        #puts  username, user.location
-	        if user.location and user.location.match("[Vv]iet")
-	            puts username
-	            $my_set.add(username)
-	        end
-	        
-	        }
 
+users = gets.chomp
+
+def query(users)
+  followers = $client.followers("#{users}")[0..2].map { |follower|
+    follower[:login]
+  }
+
+  new_followers = followers.select { |follower| !$my_set.include? (follower) }
+
+  new_followers.each { |username|
+
+    puts 'I am running ' + username
+
+    $my_set.add(username)
+    
+    user = $client.user username
+
+    if user.location and user.location.match("[Vv]iet")
+      puts username
+    end
+    
+  }
 end
+
 while 1 do
-	query("#{users}").each { |result| 
-		query(result)
-}
+  query("#{users}").each { |result| 
+    query(result)
+  }
 end 
